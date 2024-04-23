d086b152f6c8641092ea40f987ad405a50d2aa38
Nibronix <nicholas.marolla@gmail.com>
2024-04-22 21:00:14 -0700

    Incorporated the Fire Spell scale up as an item, which will also delete itself. Must be added on an Area2D node.

--- Scripts/Items/FireSpellScaleUp.gd
+++ Scripts/Items/FireSpellScaleUp.gd
@@ -3,7 +3,8 @@ extends Area2D
 func _ready():
 	self.body_entered.connect(Callable(self, "_on_body_entered"))
 
-# Increase size by 10%
+# Increase size by 10% and delete self
 func _on_body_entered(_body: PhysicsBody2D) -> void:
 	var fire_scale = get_node("/root/Level Scene/Player/PlayerWeapons/FireSpell")
 	fire_scale.scale_factor *= 1.1
+	self.queue_free()
