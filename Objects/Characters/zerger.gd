9a16f1aa8c29fd7b1b6eadb2307fffc0169e7b1e
Nibronix <nicholas.marolla@gmail.com>
2024-05-08 17:00:34 -0700

    Merged conflicts

--- Objects/Characters/zerger.gd
+++ Objects/Characters/zerger.gd
@@ -1,11 +1,41 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 20 # Speed of the enemy
+var health = 30 # Health of the enemy
 
 func _ready():
 	add_to_group("Entities")
+	$AnimationPlayer.play("Flying")
+	$Hurtbox.connect("area_entered", Callable(self, "_on_Hurtbox_area_entered"))
 
 func _physics_process(delta):
 	var player = get_node("/root/Level Scene/Player")
 	var direction = (player.global_position - global_position).normalized()
+	velocity = direction * speed
 	move_and_slide()
+
+func _on_Hurtbox_area_entered(area):
+	var player = get_node("/root/Level Scene/Player")
+	var weapons_node = player.get_node_or_null("Weapons")
+	if weapons_node:
+		if area.get_parent().get_parent() == weapons_node:
+			var damage = 0
+			if area.get_parent().name == "FireSpell":
+				damage = player.get_node("PlayerStats").FireDamage
+			elif area.get_parent().name == "Lance":
+				damage = player.get_node("PlayerStats").LanceDamage
+			take_damage(damage)
+
+# This function is called by the player's attack when it overlaps with this enemy.
+func take_damage(damage_amount):
+	health -= damage_amount
+	if health <= 0:
+		die()
+
+# Handles the death of the enemy.
+func die():
+	queue_free()
+
+# will remove after
+func _process(delta):
+	print("Current Health: ", health)
